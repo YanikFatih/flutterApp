537bf61ea0d3588e92d83e04fcc1335be59da953
Fatih Yanık <69479457+YanikFatih@users.noreply.github.com>
2023-08-23 14:39:41 +0300

    pokemon service added

--- lib/service/pokemon_service.dart
+++ lib/service/pokemon_service.dart
@@ -1,12 +1,11 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:idenfit_my_first_app/configs/constants.dart';
 import 'package:idenfit_my_first_app/model/pokemon_model.dart';
 
 class PokemonService {
-  final String apiUrl = 'https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json';
-
   Future<PokemonModel> fetchPokemons() async {
-    final response = await http.get(Uri.parse(apiUrl));
+    final response = await http.get(Uri.parse(pokemonApiUrl));
     if (response.statusCode == 200) {
       var jsonBody =  PokemonModel.fromJson(json.decode(response.body));
       return jsonBody;
