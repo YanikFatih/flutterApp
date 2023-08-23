d6cce975d5f0ca3d6284a358d698b7328e9e5f05
Fatih Yanık <69479457+YanikFatih@users.noreply.github.com>
2023-08-23 14:38:23 +0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:idenfit_my_first_app/pages/splashScreen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:idenfit_my_first_app/pages/splashScreen.dart';
 
 void main() {
   runApp(
